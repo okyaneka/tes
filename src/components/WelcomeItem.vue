42a9a9f4a5ab69e7c31be82323b337f722cd5497
Oky Aneka <okyaneka@gmail.com>
2022-06-28 16:49:14 +0700

    Init commit

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="p-4 flex items-center gap-4 rounded-md border border-green-200 hover:bg-green-50"
+  >
+    <i class="block text-green-700">
+      <slot name="icon"></slot>
+    </i>
+    <div class="">
+      <div class="text-xl font-semibold text-green-700">
+        <slot name="heading"></slot>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
